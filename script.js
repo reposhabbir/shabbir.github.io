0595ea4ea1749c1ee7376156b1dc57d78a2e75e3
reposhabbir <syedshabbir91827@gmail.com>
2025-02-17 23:28:54 +0530

    Create script.js
    added valid script js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Example of smooth scrolling to section on nav item click
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
